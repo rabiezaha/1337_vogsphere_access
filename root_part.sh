def4d1ad0cb4c31869b0d40ddf0ce334431ae464
ZAHA RABIE <rabiezaha@gmail.com>
2019-10-15 19:38:13 +0200

    root_part.sh

--- root_part.sh
+++ root_part.sh
@@ -1,47 +1,39 @@
 #!/bin/bash
 
-### We set up the kerberos config file to access to 42's kerberos server.
+### We set up the kerberos config file to access to 1337's kerberos server.
 
-mv /etc/krb5.conf /etc/krb5.conf_before42vogsphereaccess
+mv /etc/krb5.conf /etc/krb5.conf_before1337vogsphereaccess
 
-echo "Original /etc/krb5.conf renamed : /etc/krb5.conf_before42vogsphereaccess" 
+echo "Original /etc/krb5.conf renamed : /etc/krb5.conf_before1337vogsphereaccess" 
 
 cat >> /etc/krb5.conf << EOF
 [libdefaults]
-	default_realm = 42.FR
-	forwardable = true
-	proxiable = true
-      	dns_lookup_kdc = no
-      	dns_lookup_realm = no
-	allow_weak_crypto = true
+    default_realm = 1337.MA
+    forwardable = true
+    proxiable = true
+    dns_lookup_kdc = no
+    dns_lookup_realm = no
+    allow_weak_crypto = true
 
 [realms]
-	42.FR = {
-                kdc = kdc1.42.fr
-                kdc = kdc2.42.fr
-                admin_server = kdc1.42.fr
-		default_domain = 42.fr
-		default_lifetime = 7d
-                ticket_lifetime = 7d
-	}
-        STAFF.42.FR = {
-                kdc = dc1.staff.42.fr
-                admin_server = dc1.staff.42.fr
-                default_domain = staff.42.fr
-        }
+    1337.MA = {
+        kdc = ldap.1337.ma
+        admin_server = ldap.1337.ma
+        default_domain = 1337.ma
+        default_lifetime = 7d
+        ticket_lifetime = 7d
+    }
 
 [domain_realm]
-	.42.fr = 42.FR
-	42.fr = 42.FR
-	.staff.42.fr = STAFF.42.FR
-	staff.42.fr = STAFF.42.FR
+    .1337.ma = 1337.MA
+    1337.ma = 1337.MA
 EOF
 
 ### We set up the ssh config file to access to 42's git server (vogsphere)
 ### using your kerberos identification
 
 cat >> /etc/ssh/ssh_config << EOF
-Host *.42.fr
+Host *.1337.ma
      SendEnv LANG LC_*
      StrictHostKeyChecking no
      GSSAPIAuthentication yes
